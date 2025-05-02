8332216a796d7aac8e206f5907a9e368d9849409
German <maurino.german@gmail.com>
2025-05-02 17:59:30 -0300

    Reglas de negocios agregadas

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ namespace Dsw2025Ej8
     {
         static void Main(string[] args)
         {
-            CuentaBancaria cb = new CuentaBancaria("124", 124, TipoCuenta.CajaDeAhorro, ["german","javier"]);
-            cb.TasaDeInteres = 0.2M;
-            cb.LimiteDeDescubierto = 0.10M;
+            CuentaBancaria cb = new CuentaBancaria("124", 0, TipoCuenta.CuentaCorriente, ["german", "javier"])
+            {
+                TasaDeInteres = 0.2M,
+                LimiteDeDescubierto = 0.10M
+            };
+            
             Console.WriteLine($"Cuenta Bancaria: {cb.Numero} , {cb.Saldo} , {cb.Tipo} , {cb.Titulares[0]}, {cb.TasaDeInteres}, {cb.LimiteDeDescubierto}");
 
+            //cb.Depositar(110);
+            cb.Retirar(200);
         }
         
     }
